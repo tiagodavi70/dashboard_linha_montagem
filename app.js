20fcc093d9398637b0eea61a55c3f9d093fcb7d6
Tiago Araújo <tiagodavi70@gmail.com>
2023-02-27 20:50:15 +0000

    added index to sim and corrected some visual glitches

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let station_selected = -1;
 let mainColor = "rgb(18, 36, 110)";
 let selectionColor = "rgb(111, 111, 111)";
-let mode = 0; // 0 - map, 1 - overview, 2 - feature, 3 - sample
+let mode = 0; // 0 - map, 1 - kpi, 2 - cycle, 3 - sample kpi, 4 - sample cycle times
 let lastAttr = 0;
 let specs = [];
 
@@ -11,40 +11,27 @@ let kpis = {"00": "oee", "01": "fpy", "10": "partCount", "11": "partCountSetPoin
 
 
 d3.select("#close_icon").on("click", function(event, d) {
-    // const e = station_list.nodes();
-    // const i = e.indexOf(this);
-
     mode = 0;
     d3.select("#svgLoaded").style("display", "block");
     d3.select("#vis-container").style("display", "none");
-
-    // switch(mode) {
-    //     case 1:{
-    //         mode = 0;
-    //         // loadVis(0);
-    //         break;
-    //     }
-    //     case 2:{
-    //         loadVis(1);
-    //         break;
-    //     }
-    //     case 3: {
-    //         loadVis(2);
-    //         break;
-    //     }
-    //     default: {}
-    // }
 });
 
 d3.select("#features_icon").on("click", function(event, d) {
-    if (mode == 0) {
+    if (station_selected != -1) { // mode == 0
         mode = 1;
         d3.select("#svgLoaded").style("display", "none");
         d3.select("#vis-container").style("display", "block");
-
         loadVis(mode);
     }
-    d3.select("#interaction-menu").style("display", "block");
+});
+
+d3.select("#cycle_icon").on("click", function(event, d) {
+    if (station_selected != -1) {
+        mode = 2;
+        d3.select("#svgLoaded").style("display", "none");
+        d3.select("#vis-container").style("display", "block");
+        loadVis(mode);
+    }
 });
 
 d3.select("#filter_icon").on("click", function(event, d) {
@@ -58,8 +45,8 @@ d3.select("#filter_icon").on("click", function(event, d) {
 
 
 function loadSpecs() {
-    Promise.all([d3.json("specs/specOverview.json"), d3.json("specs/specKPI.json"),
-                 d3.json("specs/specCycleTime.json"),d3.json("specs/specSampleKPI.json"), d3.json("specs/specSampleTime.json")]
+    Promise.all([d3.json("specs/specKPI.json"), d3.json("specs/specCycleTime.json"),
+                 d3.json("specs/specSampleKPI.json"), d3.json("specs/specSampleTime.json")]
     ).then((values) => {
         for (let i = 0; i < values.length; i++) {
             let v = values[i];
@@ -73,19 +60,15 @@ function sn(node) { return d3.select(node).attr("id").substring(1) }
 
 function createList(stations) {
     let status_size = 15;
-    let helper_f = (station, data) => {
-        let filtered = data.filter(d => d.station == station);
-        let redCount = 0;
-        for (let k of Object.keys(filtered[0])) {
-            if (!["shiftIdentifier", "locationId", "validFrom", "validTo", "station", "id", "line", "serialNumber", "reference", "timeStamp", "index"]
-                    .includes(k)) {
-                redCount = filtered.filter(d => d[k] < .001).length;
-            }
-        }
-        return { "isRed": redCount > 0, "color": redCount > 0 ? "crimson": "seagreen", "size": redCount > 0 ? status_size/2: status_size/4};
+    let helper_f = (station, type) => {
+        // let filtered = data.filter(d => d.station == station);
+        // let redCount = 0;
+        let redcheck = {"260": {"cycle": true, "kpi":true}, "270": {"cycle": false, "kpi":false}, "290": {"cycle": false, "kpi":false}}
+        let isRed = redcheck[station][type];
+        return { "isRed": isRed, "color": isRed ? "crimson": "seagreen", "size": isRed ? status_size/2 : status_size/4};
     };
-    let isBootleneck = (station) => {
-        let isRed = helper_f(station, kpi_data).isRed || helper_f(station, times_data).isRed;
+    let isBootleneck = (station) => {   
+        let isRed = station == bottleneck.station;
         return { "isRed": isRed, "color": isRed ? "crimson": "seagreen", "size": isRed ? status_size/2: status_size/4}
     }
     
@@ -107,7 +90,7 @@ function createList(stations) {
                 <div>
                     <svg width="${status_size}" height="${status_size}">
                         <circle class="status_circle" id="status_cycle_time_${sn(d)}" 
-                            cx="${status_size/2}" cy="${status_size/2}" r="${helper_f(sn(d), kpi_data).size}" style="fill: ${helper_f(sn(d), kpi_data).color}; stroke: black;"/>
+                            cx="${status_size/2}" cy="${status_size/2}" r="${helper_f(sn(d), "cycle").size}" style="fill: ${helper_f(sn(d), "cycle").color}; stroke: black;"/>
                     </svg>
                 </div>
                 <div>
@@ -119,7 +102,7 @@ function createList(stations) {
                 <div>
                     <svg width="${status_size}" height="${status_size}">
                         <circle class="status_circle" id="status_kpi${sn(d)}" 
-                            cx="${status_size/2}" cy="${status_size/2}" r="${helper_f(sn(d), times_data).size}" style="fill: ${helper_f(sn(d), times_data).color}; stroke: black;"/>
+                            cx="${status_size/2}" cy="${status_size/2}" r="${helper_f(sn(d), "kpi").size}" style="fill: ${helper_f(sn(d), "kpi").color}; stroke: black;"/>
                     </svg>
                 </div>
             `).on("click", function(event, d) {
@@ -151,16 +134,6 @@ function selectStation(stationNumber) {
         station_selected = -1;
     else
         station_selected = stationNumber;
-    
-    if (mode == 0) {
-        // mode = 1;
-        // d3.select("#svgLoaded").style("display", "none");
-        // d3.select("#vis-container").style("display", "block");
-        // loadVis(mode);
-    } else {
-        
-        // loadVis(mode, lastAttr);
-    }
 }
 
 function clearSelection() {
@@ -171,15 +144,16 @@ function clearSelection() {
 function loadVis(vistype, attr) {
     if (vistype == 0) return;
 
-    let specfilter = JSON.parse(JSON.stringify(specs[vistype - 1]));
-    if (specfilter.id == 2 || specfilter.id == 3) {
-        console.log(specfilter.data.url.includes("KPI"), specfilter.data.url)
+    let specfilter = JSON.parse(JSON.stringify(specs[vistype-1]));
+    console.log(vistype, specfilter.id)
+    if (specfilter.id == 1 || specfilter.id == 2) {
+        specfilter.transform[1] = {"filter": `datum.station == ${station_selected}`};
+        // console.log(specfilter.datasets["data-kpi"]);
 
-        specfilter.data.url = "http://localhost:5500" + (specfilter.data.url.includes("KPI") ?  `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
-        specfilter.transform[0].from.data.url = "http://localhost:5500" + (specfilter.transform[0].from.data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
+        specfilter.datasets["data-kpi"] = kpi_station[station_selected];
+        specfilter.datasets["data-times"] = cycle_station[station_selected];
 
-        specfilter.transform[1] = {"filter": `datum.station == ${station_selected}`};        
-    } else if (specfilter.id == 4 || specfilter.id == 5){
+    } else if (specfilter.id == 3 || specfilter.id == 4){
         specfilter.transform[0] = {"filter": `datum.station == ${station_selected}`};
         
         specfilter.layer[0].transform[0] = {"filter": `datum.station == ${station_selected}`};
@@ -202,7 +176,7 @@ function loadVis(vistype, attr) {
             result.view.addEventListener('click', function(event, item) {
                 
                 if (item.datum != undefined) {
-                    if (spec.id == 1) {
+                    if (spec.id == 0) {
                         let index = +item.mark.name["concat_".length];
                         switch (index){
                             case 0:{
@@ -214,9 +188,10 @@ function loadVis(vistype, attr) {
                                 break;
                             }
                         }
-                    } else if (spec.id == 2 || spec.id == 3) {
+                    } else if (spec.id == 1 || spec.id == 2) {
+                        console.log(item.datum)
                         let key_index = (item.mark.name["concat_".length]) + (item.mark.name["concat_x_concat_".length])
-                        let samples = {"4": kpis, "5": cycleTimes};
+                        let samples = {"3": kpis, "4": cycleTimes};
                         loadVis(+spec.id + 2, samples[""+(+spec.id + 2)][key_index])
                     }
                 } 
@@ -225,29 +200,29 @@ function loadVis(vistype, attr) {
     }
     // console.log("loading vis ", mode);
     
-    if (specfilter.id == 4 || specfilter.id == 5){
+    // if (specfilter.id == 3 || specfilter.id == 4){
 
-        d3.json("http://localhost:5500" + (specfilter.layer[0].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`)).then(r_data =>{
+    //     d3.json("http://localhost:5500" + (specfilter.layer[0].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`)).then(r_data =>{
 
-            delete specfilter.layer[0].data.url
-            delete specfilter.layer[1].data.url
-            delete specfilter.data.url
+    //         delete specfilter.layer[0].data.url
+    //         delete specfilter.layer[1].data.url
+    //         delete specfilter.data.url
 
-            let data = r_data.map( (d,i)=> {d.index = i; return d});
+    //         let data = r_data.map( (d,i)=> {d.index = i; return d});
 
-            specfilter.data.values = data;
-            specfilter.layer[0].data.values = data; //"http://localhost:5500" + (specfilter.layer[0].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
-            specfilter.layer[1].data.values = data; //"http://localhost:5500" + (specfilter.layer[1].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
-            // specfilter.transform[0].from.data.url = specfilter.transform[0].from.data.url.includes("KPI") ? `./simulation/KPI.json` : `./simulation/CycleTimes.json`;
+    //         specfilter.data.values = data;
+    //         specfilter.layer[0].data.values = data; //"http://localhost:5500" + (specfilter.layer[0].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
+    //         specfilter.layer[1].data.values = data; //"http://localhost:5500" + (specfilter.layer[1].data.url.includes("KPI") ? `/simulation/KPI.json` : `/simulation/CycleTimes.json`);
+    //         // specfilter.transform[0].from.data.url = specfilter.transform[0].from.data.url.includes("KPI") ? `./simulation/KPI.json` : `./simulation/CycleTimes.json`;
 
-            // specfilter.layer[0].encoding["y"].field = attr;
-            // specfilter.layer[1].encoding["y"].field = attr;
+    //         // specfilter.layer[0].encoding["y"].field = attr;
+    //         // specfilter.layer[1].encoding["y"].field = attr;
     
-            vis(specfilter, attr);
-        })
-    } else {
+    //         vis(specfilter, attr);
+    //     })
+    // } else {
         vis(specfilter, attr);
-    }
+    // }
     
     // function loadData() {
         
--- datagenerator.mjs
+++ datagenerator.mjs
@@ -31,31 +31,31 @@ let u = btoa(`${username}:${password}`);
 
 let url1 = "https://ews-emea.api.bosch.com/it/application/api/augmanity-pps4-dummy/d/v1/api/kpi/cycle-times?line=10&station=220";
 
-d3.json(url1, {
-    headers: {"Authorization": `Basic ${u}`}
-}).then(data => {
-    console.log(JSON.stringify(data));
-});
+// d3.json(url1, {
+//     headers: {"Authorization": `Basic ${u}`}
+// }).then(data => {
+//     console.log(JSON.stringify(data));
+// });
 
 let url = names[0]; // bottleneck
 let bottleneck_dataset = [];
 
 function datagen() {
-    let c260 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes260.json`)).map(d => { d.station=260;  return d})
-    let c270 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes270.json`)).map(d => { d.station=270;  return d})
-    let c290 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes290.json`)).map(d => { d.station=290;  return d})
+    let c260 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes260.json`)).map((d,i) => { d.station=260; d.index=i;  return d})
+    let c270 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes270.json`)).map((d,i) => { d.station=270; d.index=i;  return d})
+    let c290 = JSON.parse(fs.readFileSync(`./DadosSimulados/CycleTimes290.json`)).map((d,i) => { d.station=290; d.index=i;  return d})
     let cycledata = c260.concat(c270).concat(c290);
     // fs.writeFileSync(`./simulation/CycleTimes.csv`, d3.csvFormat(cycledata));
     fs.writeFileSync(`./simulation/CycleTimes.json`, JSON.stringify(cycledata));
 
-    let k260 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries260.json`)).map(d => { d.station=260;  return d})
-    let k270 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries270.json`)).map(d => { d.station=270;  return d})
-    let k290 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries290.json`)).map(d => { d.station=290;  return d})
+    let k260 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries260.json`)).map((d,i) => { d.station=260; d.index=i;  return d})
+    let k270 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries270.json`)).map((d,i) => { d.station=270; d.index=i;  return d})
+    let k290 = JSON.parse(fs.readFileSync(`./DadosSimulados/KPITimeSeries290.json`)).map((d,i) => { d.station=290; d.index=i;  return d})
     let kpidata = k260.concat(k270).concat(k290);
     // fs.writeFileSync(`./simulation/KPI.csv`, d3.csvFormat(kpidata));
     fs.writeFileSync(`./simulation/KPI.json`, JSON.stringify(kpidata));
 }
-// datagen();
+datagen();
 
 // fs.readFile(`./datasetsrafael/${url}.json`, (err, data) => {
 //     if (err) throw err;
